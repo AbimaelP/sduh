83c16c510efe2754ca9ee3a48d8638b832d5e318
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-08-29 15:20:42 -0300

    Acesso de admin regras

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -19,7 +19,7 @@ export const AuthProvider = ({ children }) => {
         break;
 
       case 'cidadao':
-        userData = { user: 'Cidadão', role: 'cidadao', main_role: 'admin' };
+        userData = { user: 'Cidadão', role: 'cidadao', main_role: 'cidadao' };
         break;
     }
 
