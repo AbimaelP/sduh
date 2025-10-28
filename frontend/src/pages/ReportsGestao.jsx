ffe1b9bc06ad4a27380c93c9d7c5a0c5acaa45e4
a.bimael2000@hotmail.com <a.bimael2000@hotmail.com>
2025-10-28 14:46:30 -0300

    ajuste de relatorio

--- frontend/src/pages/ReportsGestao.jsx
+++ frontend/src/pages/ReportsGestao.jsx
@@ -3,7 +3,7 @@ import LayoutClient from "../layouts/LayoutClient";
 export default function ReportsGestao() {
   return (
     <LayoutClient>
-        <iframe src="https://lookerstudio.google.com/embed/reporting/41164f8d-3a25-4e8c-97c7-ac349b9e3dfe/page/r4NVF" frameborder="0" className="report-gestao-iframe"></iframe>
+        <iframe src="https://lookerstudio.google.com/embed/reporting/5756095b-0b28-42b9-a27e-09de5e988aef/page/r4NVF" frameborder="0" className="report-gestao-iframe"></iframe>
     </LayoutClient>
   );
 }
